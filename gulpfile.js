87dbeacc72e818a431f9781f6a55a4fd25190454
Max <masamune.code@gmail.com>
2019-09-05 19:39:36 +0200

    ADD: SASS & example

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,22 @@ const {
   src,
   task
 } = require('gulp');
-
+const sass = require('gulp-sass');
 
 // // Sub-tasks
 
 // Copy all assets to dist
-task('copy-assets', () => src(['src/**/*.*', '!node_modules/**'])
+task('copy-assets', () => src(['src/**/*.*', '!node_modules/**', '!**/*.scss'])
   .pipe(dest('dist/')));
 
+task('sass', function() {
+  return src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/'));
+});
 
 // // Tasks
 
 // Default tesk, executed when using 'gulp'
 //    Capy all assets to dist
-task('default', parallel('copy-assets'));
+task('default', parallel('copy-assets', 'sass'));
